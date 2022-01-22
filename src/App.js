db48f6cc5a155a7e81a0d7a5c8123d4f33c3c5e7
l33th <l33th@protonmail.ch>
2022-01-22 06:41:09 +0200

    added useEffect setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    const fetchJoke = async () => {
+      await fetch(`http://api.icndb.com/jokes/random?firstName=L33th&lastName=AI`);
+    };
+  }, []);
+
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Joke Generator Setup</h1>
       <button>Generate Joke</button>
     </div>
