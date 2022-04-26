6d8da6f502abe51fb98adb9a947c72adbd7a3f74
l33th <l33th@protonmain.ch>
2022-04-26 09:46:48 +0200

    added first and last name state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
+import { useState } from 'react';
 import './App.css';
 import useRandomJoke from './Hooks/useRandomJoke/useRandomJoke';
 
 function App() {
   const joke = useRandomJoke('Dude', 'AI');
 
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
   return (
     <div className='App'>
       <center>
         <h1>Joke Generator Setup</h1>
         <h2>{joke}</h2>
-        <button>Generate Joke</button>
+
+        <form action=''>
+          <input placeholder='first name' value={firstName} onChange={e => setFirstName(e.target.value)} />
+          <input placeholder='last name'  value={lastName} onChange={e => setLastName(e.target.value)} />
+          <button>Generate Joke</button>
+        </form>
       </center>
     </div>
   );
